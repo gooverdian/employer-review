a245341adfa10d52a25f276798875cae99321b08
colorbash <colorbash3000@gmail.com>
2018-03-12 13:03:12 +0300

    Added sources to download at least 5000 employers.

---
+++ src/main/java/response/LogoUrlsJSON.java
@@ -0,0 +1,15 @@
+package response;
+
+import com.google.gson.annotations.SerializedName;
+
+public class LogoUrlsJSON {
+
+    @SerializedName(value = "90")
+    String logo90;
+
+    @SerializedName(value = "240")
+    String logo240;
+
+    @SerializedName(value = "original")
+    String logoOriginal;
+}
