2bede7c0f0ac8b325117c05df16dbc82ae9e2522
Sergey Badanin <b.s.ya@yandex.ru>
2018-03-28 02:00:50 +0300

    Исправление Employer

---
+++ scripts/create_employers_and_area_tables.sql
@@ -0,0 +1,22 @@
+CREATE TABLE employer (
+    id                  SERIAL PRIMARY KEY,
+    hh_id               int UNIQUE,
+    name                varchar NOT NULL,
+    site_url            varchar,
+    description         varchar,
+    branded_descrption  varchar,
+    alternate_url       varchar,
+    logo_url_90         varchar,
+    logo_url_240        varchar,
+    logo_url_original   varchar,
+    area_id             integer,
+    area_name           varchar,
+    score               real,
+    people_rated        int
+);
+
+CREATE TABLE area (
+	id int PRIMARY KEY,
+	parent_id integer,
+	name varchar NOT NULL
+);
