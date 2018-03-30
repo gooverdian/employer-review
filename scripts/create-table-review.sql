423ac348ccd1a44ea0e3df59539e7033f87e9706
Irina Petrova <iraloseva@bk.ru>
2018-03-30 12:15:39 +0300

    Flyway example
    cherry-pick from 58db2284f3ca0066784744bf22b3632f0be61927

--- scripts/create-table-review.sql
+++
@@ -1,8 +0,0 @@
---Отзыв
-CREATE TABLE review (
-	id SERIAL PRIMARY KEY,
-	employer_id integer NOT NULL REFERENCES employer,
-	created_on timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	text varchar,
-	rating real NOT NULL
-);
---
+++ scripts/src/main/resources/db/migration/V2__insert_first_data.sql
@@ -0,0 +1,3 @@
+insert into test22 (ID, NAME) values (1, 'Axel');
+insert into test22 (ID, NAME) values (2, 'Mr. Foo');
+insert into test22 (ID, NAME) values (3, 'Ms. Bar');
